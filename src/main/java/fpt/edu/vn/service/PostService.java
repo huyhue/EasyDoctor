2770af63c36d1d2f4b5fd8f5081b8219804eca3e
huyhue <tpgiahuy5@gmail.com>
2022-07-23 22:44:34 +0700

    ok comit

--- src/main/java/fpt/edu/vn/service/PostService.java
+++ src/main/java/fpt/edu/vn/service/PostService.java
@@ -5,6 +5,5 @@ import java.util.List;
 import fpt.edu.vn.component.PostDTO;
 
 public interface PostService {
-//	List<PostDTO> getListPost(String keyword, Integer special_id);
 	List<PostDTO> getListPosts();
 }
--- src/main/java/fpt/edu/vn/service/impl/PostServiceImpl.java
+++ src/main/java/fpt/edu/vn/service/impl/PostServiceImpl.java
@@ -1,27 +1,18 @@
 package fpt.edu.vn.service.impl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
 
-import fpt.edu.vn.component.CommentDTO;
 import fpt.edu.vn.component.PostDTO;
 import fpt.edu.vn.model.Post;
 import fpt.edu.vn.repository.PostRepository;
 import fpt.edu.vn.service.PostService;
-import fpt.edu.vn.util.Utils;
 
 @Service
 public class PostServiceImpl implements PostService{
 	
 	private final PostRepository postRepository;
-//	@PersistenceContext
-//    EntityManager entityManager;
 	
     public PostServiceImpl(PostRepository postRepository) {
 		super();
@@ -31,87 +22,7 @@ public class PostServiceImpl implements PostService{
     public List<PostDTO> getListPosts(){
     	 List<PostDTO> list = new ArrayList<>();
     	 List<Post> listPost =  postRepository.findAll();
-    	
     	 return list;
     }
 
-//    public List<PostDTO> getListPost(String keyword, Integer special_id) {
-////        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-////        CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
-//        List<PostDTO> ls = new ArrayList<>();
-//        String condition = "";
-//        if (special_id != null) {
-//            condition += "special_id=" + special_id;
-//        }
-//        if (keyword != null) {
-//            if (condition.length() > 0)
-//                condition += " and ";
-//            condition += "message like '%" + keyword + "%'";
-//        }
-//        if (condition.length() > 0)
-//            condition = " where " + condition;
-//        String sql = "SELECT p.id,u.fullname,u.profile_img,s.name,p.message,p.img,likes,total_like,update_at FROM easydoctor.posts p join specialties s on s.id=p.special_id join users u on p.user_id=u.id "
-//                + condition;
-//        List<Object[]> reList = entityManager.createNativeQuery(sql).getResultList();
-//        ls = reList.stream().map(obj -> {
-//            PostDTO p = new PostDTO();
-//            p.setId(Utils.objToLong(obj[0]));
-//            p.setUsername(Utils.objToString(obj[1]));
-//            p.setUserImg(Utils.objToString(obj[2]));
-//            p.setSpecial(Utils.objToString(obj[3]));
-//            p.setMessage(Utils.objToString(obj[4]));
-//            p.setImg(Utils.objToString(obj[5]));
-//            p.setLiked(isLike(Utils.objToString(obj[6]), 2));
-//            p.setTotalLike(Utils.objToInt(obj[7]));
-//            p.setComments(getComment(p.getId()));
-//            p.setTime(Utils.objToString(obj[8]));
-//            return p;
-//        }).collect(Collectors.toList());
-//        return ls;
-//    }
-//
-//    private List<CommentDTO> getComment(long id) {
-//        String sql = "SELECT c.id,message,fullname,update_at,u.profile_img,c.user_id FROM easydoctor.comments c join users u on u.id=c.user_id where post_id="
-//                + id+" order by update_at desc";
-//        List<Object[]> ls = entityManager.createNativeQuery(sql).getResultList();
-//        List<CommentDTO> lsCom = ls.stream().map(obj -> {
-//            CommentDTO c = new CommentDTO(Utils.objToLong(obj[0]), Utils.objToString(obj[1]), Utils.objToString(obj[2]),
-//                    Utils.objToString(obj[3]), Utils.objToString(obj[4]), Utils.objToLong(obj[5]));
-//            return c;
-//        }).collect(Collectors.toList());
-//        return lsCom;
-//    }
-
-//    public boolean handleLike(long postId) {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
-//        try {
-//            Post p = postRepository.getById(postId);
-//            String lsLike = p.getLikes();
-//            String newLs = "";
-//            if (lsLike.contains(user.getId().toString())) {
-//                if (lsLike.length() > 1) {
-//                    newLs = lsLike.replace("," + user.getId(), "");
-//                }
-//                p.setLikes(newLs);
-//                postRepository.save(p);
-//                return true;
-//            }
-//            if (lsLike.length() > 1) {
-//                newLs = lsLike + "," + user.getId();
-//            } else {
-//                newLs = user.getId() + "";
-//            }
-//            p.setLikes(newLs);
-//            postRepository.save(p);
-//        } catch (Exception e) {
-//            return false;
-//        }
-//        return true;
-//    }
-
-//    public boolean isLike(String likes, int id) {
-//        List<String> ls = Arrays.asList(likes.split(","));
-//        return ls.contains(String.valueOf(id));
-//    }
 }
